807127f1ec402e2c5741ec8f2ad38e00763d1537
Sam Kester <samkester@gmail.com>
2020-08-07 09:17:17 -0600

    added Header function and append header to page

--- components/Header.js
+++ components/Header.js
@@ -11,4 +11,23 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header(){
+    const headerBody = document.createElement("div");
+    headerBody.classList.add("header");
+
+    const date = document.createElement("span");
+    date.classList.add("date");
+    date.textContent = "MARCH 28, 2020";
+
+    const title = document.createElement("h1");
+    title.textContent = "Lambda Times";
+
+    const temp = document.createElement("span");
+    temp.classList.add("temp");
+    temp.textContent = "98°";
+
+    headerBody.append(date, title, temp);
+    return headerBody;
+}
+
+document.querySelector("div.header-container").append(Header());
