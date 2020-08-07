fec1a7ef0d53400424c44a708f1c983f4006f71b
Sam Kester <samkester@gmail.com>
2020-08-07 10:26:56 -0600

    created carousel; carousel appears on the page

--- components/Carousel.js
+++ components/Carousel.js
@@ -22,3 +22,49 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+function carousel(...paths){ // allows any number of image paths to be passed to one argument
+  const carousel = document.createElement("div");
+  carousel.classList.add("carousel");
+
+  const leftButton = document.createElement("div");
+  leftButton.classList.add("left-button");
+  leftButton.textContent = " < ";
+
+  const rightButton = document.createElement("div");
+  rightButton.classList.add("right-button");
+  rightButton.textContent = " > ";
+
+  const images = paths.map(path => {
+    const image = document.createElement("img");
+    image.src = path;
+    image.style = "transition: opacity 1s; display: initial; width: 100%";
+    carousel.append(image);
+    return image;
+  });
+
+  console.log(images);
+
+  let currentIndex = 0;
+
+  function hideAllButCurrentIndex(){
+    images.forEach((item, index) => {
+      if(currentIndex === index) item.style.opacity = "100%";
+      else item.style.opacity = "0%";
+    });
+  }
+
+  hideAllButCurrentIndex();
+
+  carousel.prepend(leftButton);
+  carousel.append(rightButton);
+
+  return carousel;
+}
+
+document.querySelector(".carousel-container").append(carousel(
+  "https://tk-assets.lambdaschool.com/ba687af4-3a1e-43d7-87b2-f30453264c9d_mountains.jpeg",
+  "https://tk-assets.lambdaschool.com/8aa075b0-67cf-47ce-9a7f-8cc9d754675d_computer.jpeg",
+  "https://tk-assets.lambdaschool.com/5b7441c6-6e4b-4feb-a4ec-8dd2eb76238a_trees.jpeg",
+  "https://tk-assets.lambdaschool.com/0b770382-d0eb-4465-8bf2-692a79fcda71_turntable.jpeg"
+));
