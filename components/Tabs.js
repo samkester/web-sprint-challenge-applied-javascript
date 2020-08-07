ad6c1b6d8637c8562db2bff5a06dfa366b22f9a6
Sam Kester <samkester@gmail.com>
2020-08-07 09:22:28 -0600

    added Tabs handler

--- components/Tabs.js
+++ components/Tabs.js
@@ -9,3 +9,21 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+
+import axios from "axios";
+
+const tabContainer = document.querySelector("div.topics");
+
+axios.get("https://lambda-times-api.herokuapp.com/topics").then(response => {
+    /*console.log(response);
+    console.log(response.data);
+    console.log(response.data.topics);
+    console.log(response.data.topics[0]);*/
+
+    response.data.topics.forEach(item => {
+        const div = document.createElement("div");
+        div.textContent = item;
+        div.classList.add("tab");
+        tabContainer.append(div);
+    });
+});
